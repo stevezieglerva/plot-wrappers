2296efec8904b2bdb95efe6b261758ad89546f2c
Steve Ziegler <stephen.v.ziegler@gmail.com>
2021-02-15 12:31:33 -0500

    support count vs sum

--- tests/StackedHistogram.py
+++ tests/StackedHistogram.py
@@ -5,15 +5,19 @@ import matplotlib.pyplot as plt
 
 
 class StackedHistogram:
-    def __init__(self, date_column_name, grouping_column, value_column, df):
+    def __init__(
+        self, date_column_name, grouping_column, value_column, df, aggregation="sum"
+    ):
         self._max_groupings = 5
         self._date_column_name = date_column_name
         self._grouping_column = grouping_column
         self._value_column = value_column
+        self._aggregation = aggregation
         self._input_df = df
         self._grouped_df = self._group_data().unstack()
+        plt.style.use("seaborn")
+        self._grouped_df.plot(kind="area", stacked=True)
 
-        self._grouped_df.plot(kind="bar", stacked=True)
         # plt.show()
 
     def _group_data(self):
@@ -28,9 +32,14 @@ class StackedHistogram:
         filtered_to_largest = self._input_df[
             self._input_df[self._grouping_column].isin(largest_categories)
         ]
-        new_group = filtered_to_largest.groupby(
-            [self._date_column_name, self._grouping_column]
-        )[self._value_column].sum()
+        if self._aggregation == "sum":
+            new_group = filtered_to_largest.groupby(
+                [self._date_column_name, self._grouping_column]
+            )[self._value_column].sum()
+        if self._aggregation == "count":
+            new_group = filtered_to_largest.groupby(
+                [self._date_column_name, self._grouping_column]
+            )[self._value_column].count()
         return new_group
 
     def to_json(self):
---
+++ tests/test_special_git_log.py
@@ -0,0 +1,132 @@
+import json
+import unittest
+from unittest.mock import MagicMock, Mock, PropertyMock, patch
+
+import pandas as pd
+
+from StackedHistogram import StackedHistogram
+
+
+class GitLogUnit(unittest.TestCase):
+    def test_constructor__given_gov_uk__then_json_is_correct(self):
+        # Arrange
+        df = pd.read_csv(
+            "/Users/sziegler/Documents/Github/git-log-to-csv/samples/govuk.csv"
+        )
+        # commit_hash,epoch,timestamp,date,year,month,day,author,file,churn_count,dir_1,dir_2,dir_3,dir_4
+        # ce783c7,1576172319,2019-12-12T12:38:39,2019-12-12,2019,12,12,"Steve Ziegler",.gitignore,3,,,,
+
+        df["new_date"] = pd.to_datetime(df["date"], format="%Y-%m-%d").dt.to_period("M")
+
+        filtered = df
+        print(filtered)
+        subject = StackedHistogram(
+            "new_date", "dir_1", "churn_count", filtered, "count"
+        )
+
+        filename = "temp_dir_1_govuk.png"
+        subject.save_plot(filename)
+
+    def test_constructor__given_gov_uk__then_json_is_correct(self):
+        # Arrange
+        df = pd.read_csv(
+            "/Users/sziegler/Documents/Github/git-log-to-csv/samples/govuk.csv"
+        )
+        # commit_hash,epoch,timestamp,date,year,month,day,author,file,churn_count,dir_1,dir_2,dir_3,dir_4
+        # ce783c7,1576172319,2019-12-12T12:38:39,2019-12-12,2019,12,12,"Steve Ziegler",.gitignore,3,,,,
+
+        df["new_date"] = pd.to_datetime(df["date"], format="%Y-%m-%d").dt.to_period("M")
+        df["two_dirs"] = df["dir_1"] + "/" + df["dir_2"]
+
+        filtered = df
+        print(filtered)
+        subject = StackedHistogram(
+            "new_date", "two_dirs", "churn_count", filtered, "count"
+        )
+
+        filename = "temp_two_dirs_govuk.png"
+        subject.save_plot(filename)
+
+    def test_constructor__given_ops_aws__then_json_is_correct(self):
+        # Arrange
+        df = pd.read_csv(
+            "/Users/sziegler/Documents/Github/git-log-to-csv/samples/ops_aws_git_log.csv"
+        )
+        # commit_hash,epoch,timestamp,date,year,month,day,author,file,churn_count,dir_1,dir_2,dir_3,dir_4
+        # ce783c7,1576172319,2019-12-12T12:38:39,2019-12-12,2019,12,12,"Steve Ziegler",.gitignore,3,,,,
+
+        df["new_date"] = pd.to_datetime(df["date"], format="%Y-%m-%d").dt.to_period("M")
+        df["two_dirs"] = df["dir_1"] + "/" + df["dir_2"]
+
+        filtered = df[df["dir_1"] != "useful_scripts"]
+        print(filtered)
+        subject = StackedHistogram(
+            "new_date", "two_dirs", "churn_count", filtered, "count"
+        )
+
+        filename = "temp_ops_aws.png"
+        subject.save_plot(filename)
+
+    def test_constructor__given_ops_aws__then_json_is_correct(self):
+        # Arrange
+        df = pd.read_csv(
+            "/Users/sziegler/Documents/Github/git-log-to-csv/forem_git_log.csv"
+        )
+        # commit_hash,epoch,timestamp,date,year,month,day,author,file,churn_count,dir_1,dir_2,dir_3,dir_4
+        # ce783c7,1576172319,2019-12-12T12:38:39,2019-12-12,2019,12,12,"Steve Ziegler",.gitignore,3,,,,
+
+        df["new_date"] = pd.to_datetime(df["date"], format="%Y-%m-%d").dt.to_period("M")
+        df["two_dirs"] = df["dir_1"] + "/" + df["dir_2"]
+
+        filtered = df
+        print(filtered)
+        subject = StackedHistogram(
+            "new_date", "two_dirs", "churn_count", filtered, "count"
+        )
+
+        filename = "temp_forem.png"
+        subject.save_plot(filename)
+
+    def test_constructor__given_react__then_json_is_correct(self):
+        # Arrange
+        df = pd.read_csv(
+            "/Users/sziegler/Documents/Github/git-log-to-csv/samples/react.csv"
+        )
+        # commit_hash,epoch,timestamp,date,year,month,day,author,file,churn_count,dir_1,dir_2,dir_3,dir_4
+        # ce783c7,1576172319,2019-12-12T12:38:39,2019-12-12,2019,12,12,"Steve Ziegler",.gitignore,3,,,,
+
+        df["new_date"] = pd.to_datetime(df["date"], format="%Y-%m-%d").dt.to_period("M")
+        df["two_dirs"] = df["dir_1"] + "/" + df["dir_2"]
+
+        filtered = df
+        print(filtered)
+        subject = StackedHistogram(
+            "new_date", "two_dirs", "churn_count", filtered, "count"
+        )
+
+        filename = "temp_react.png"
+        subject.save_plot(filename)
+
+    def test_constructor__given_cw_api__then_json_is_correct(self):
+        # Arrange
+        df = pd.read_csv(
+            "/Users/sziegler/Documents/Github/git-log-to-csv/cw_api_git_log.csv"
+        )
+        # commit_hash,epoch,timestamp,date,year,month,day,author,file,churn_count,dir_1,dir_2,dir_3,dir_4
+        # ce783c7,1576172319,2019-12-12T12:38:39,2019-12-12,2019,12,12,"Steve Ziegler",.gitignore,3,,,,
+
+        df["new_date"] = pd.to_datetime(df["date"], format="%Y-%m-%d").dt.to_period("M")
+        df["two_dirs"] = df["dir_1"] + "/" + df["dir_2"]
+
+        filtered = df
+        print(filtered)
+        subject = StackedHistogram(
+            "new_date", "two_dirs", "churn_count", filtered, "count"
+        )
+
+        filename = "temp_cw_api.png"
+        subject.save_plot(filename)
+
+
+if __name__ == "__main__":
+    unittest.main()
