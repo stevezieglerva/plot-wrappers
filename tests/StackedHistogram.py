7404d34abe7bf134a1152eac820f45325ed1691c
Steve Ziegler <stephen.v.ziegler@gmail.com>
2021-02-15 09:55:27 -0500

    largest grouping works

--- tests/StackedHistogram.py
+++ tests/StackedHistogram.py
@@ -5,11 +5,29 @@ import pandas as pd
 
 class StackedHistogram:
     def __init__(self, date_column_name, grouping_column, value_column, df):
+        self._max_groupings = 5
+        self._date_column_name = date_column_name
+        self._grouping_column = grouping_column
+        self._value_column = value_column
         self._input_df = df
-        self._grouped_df = df.groupby([date_column_name, grouping_column])[
-            value_column
-        ].sum()
-        pass
+        self._grouped_df = self._group_data()
+
+    def _group_data(self):
+        largest_df = (
+            self._input_df.groupby([self._grouping_column])[self._value_column]
+            .sum()
+            .nlargest(self._max_groupings)
+            .to_frame()
+        )
+        largest_categories = largest_df.index.values.tolist()
+        print(largest_categories)
+        filtered_to_largest = self._input_df[
+            self._input_df[self._grouping_column].isin(largest_categories)
+        ]
+        new_group = filtered_to_largest.groupby(
+            [self._date_column_name, self._grouping_column]
+        )[self._value_column].sum()
+        return new_group
 
     def to_json(self):
         json_str = self._grouped_df.to_json()
--- tests/test_unit.py
+++ tests/test_unit.py
@@ -8,7 +8,7 @@ from StackedHistogram import StackedHistogram
 
 
 class StackedHistGramUnit(unittest.TestCase):
-    def test_constructor__given_df_input__then_json_is_correct(self):
+    def test_constructor__given_simple_csv__then_json_is_correct(self):
         # Arrange
         df = pd.read_csv("tests/data/stacked_simple.csv")
         subject = StackedHistogram("date", "fruit", "quantity", df)
@@ -18,6 +18,37 @@ class StackedHistGramUnit(unittest.TestCase):
         print(json.dumps(results, indent=3))
 
         # Assert
+        expected = {
+            "('2021-01-01', 'apple')": 35,
+            "('2021-01-01', 'pears')": 5,
+            "('2021-01-02', 'apple')": 10,
+            "('2021-01-02', 'pears')": 20,
+        }
+        self.assertEqual(results, expected)
+
+    def test_constructor__given_more_than_max_csv__then_json_is_correct(self):
+        # Arrange
+        df = pd.read_csv("tests/data/stacked_more_than_max.csv")
+        subject = StackedHistogram("date", "fruit", "quantity", df)
+
+        # Act
+        results = subject.to_json()
+        print(json.dumps(results, indent=3))
+
+        # Assert
+        expected = {
+            "('2021-01-01', 'apple')": 35,
+            "('2021-01-01', 'bananas')": 7,
+            "('2021-01-01', 'oranges')": 13,
+            "('2021-01-01', 'pears')": 5,
+            "('2021-01-01', 'pineapple')": 7,
+            "('2021-01-02', 'apple')": 1,
+            "('2021-01-02', 'bananas')": 6,
+            "('2021-01-02', 'oranges')": 10,
+            "('2021-01-02', 'pears')": 9,
+            "('2021-01-02', 'pineapple')": 6,
+        }
+        self.assertEqual(results, expected)
 
 
 if __name__ == "__main__":
